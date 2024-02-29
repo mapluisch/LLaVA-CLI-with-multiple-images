94c26079ec69f064dd89d368dcfd6fd4bf8f8c68
Martin Pluisch <31780571+mapluisch@users.noreply.github.com>
2024-02-29 13:18:34 +0100

    Added variable grid sizes and resolution

--- llava-multi-images.py
+++ llava-multi-images.py
@@ -10,6 +10,7 @@ from llava.mm_utils import process_images, tokenizer_image_token, get_model_name
 import requests
 from PIL import Image
 from io import BytesIO
+from math import ceil, sqrt
 from transformers import TextStreamer
 
 def load_image(image_file):
@@ -19,6 +20,33 @@ def load_image(image_file):
     else:
         image = Image.open(image_file).convert('RGB')
     return image
+
+
+def expand_image_range_paths(paths):
+    expanded_paths = []
+    # check if specified --images is range of imgs
+    for path in paths:
+        if "{" in path and "}" in path:
+            pre, post = path.split("{", 1)
+            range_part, post = post.split("}", 1)
+            start, end = map(int, range_part.split("-"))
+
+            for i in range(start, end + 1):
+                expanded_paths.append(f"{pre}{i}{post}")
+        else:
+            expanded_paths.append(path)
+
+    return expanded_paths
+
+
+def parse_resolution(resolution_str):
+    # try to parse a string into a resolution tuple for the grid output
+    try:
+        width, height = map(int, resolution_str.split(','))
+        return width, height
+    except Exception as e:
+        raise argparse.ArgumentTypeError("Resolution must be w,h.") from e
+
 
 def concatenate_images_vertical(images, dist_images):
     # calc max width from imgs
@@ -39,6 +67,7 @@ def concatenate_images_vertical(images, dist_images):
 
     return new_img
 
+
 def concatenate_images_horizontal(images, dist_images):
     # calc total width of imgs + dist between them
     total_width = sum(img.width for img in images) + dist_images * (len(images) - 1)
@@ -58,31 +87,41 @@ def concatenate_images_horizontal(images, dist_images):
 
     return new_img
 
-def concatenate_images_grid(images, dist_images, output_size=(2560, 1440)):
+
+def concatenate_images_grid(images, dist_images, output_size):
+    num_images = len(images)
+    # calc grid size based on amount of input imgs
+    grid_size = max(2, ceil(sqrt(num_images)))
+
+    cell_width = (output_size[0] - dist_images * (grid_size - 1)) // grid_size
+    cell_height = (output_size[1] - dist_images * (grid_size - 1)) // grid_size
+
     # create new img with output_size, black bg
     new_img = Image.new('RGB', output_size, (0, 0, 0))
-    # calc quadrant size for imgs, accounting for specified dist
-    quadrant_size = ((output_size[0] - 3 * dist_images) // 2, (output_size[1] - 3 * dist_images) // 2)
 
     for index, img in enumerate(images):
         # calc img aspect ratio
         img_ratio = img.width / img.height
-        # calc target quadrant aspect ratio
-        target_ratio = quadrant_size[0] / quadrant_size[1]
+        # calc target aspect ratio per cell
+        target_ratio = cell_width / cell_height
 
-        # resize img to fit in quadrant
+        # resize img to fit in cell
         if img_ratio > target_ratio:
-            new_width = quadrant_size[0]
-            new_height = int(quadrant_size[0] / img_ratio)
+            new_width = cell_width
+            new_height = int(cell_width / img_ratio)
         else:
-            new_width = int(quadrant_size[1] * img_ratio)
-            new_height = quadrant_size[1]
+            new_width = int(cell_height * img_ratio)
+            new_height = cell_height
 
         # resize img using lanczos filter
         resized_img = img.resize((new_width, new_height), Image.LANCZOS)
+
+        row = index // grid_size
+        col = index % grid_size
+
         # calc x, y offsets for img positioning
-        x_offset = (index % 2) * (quadrant_size[0] + dist_images) + (quadrant_size[0] - new_width) // 2 + dist_images
-        y_offset = (index // 2) * (quadrant_size[1] + dist_images) + (quadrant_size[1] - new_height) // 2 + dist_images
+        x_offset = col * (cell_width + dist_images) + (cell_width - new_width) // 2
+        y_offset = row * (cell_height + dist_images) + (cell_height - new_height) // 2
 
         # paste resized img in calc pos
         new_img.paste(resized_img, (x_offset, y_offset))
@@ -90,16 +129,16 @@ def concatenate_images_grid(images, dist_images, output_size=(2560, 1440)):
     return new_img
 
 
-
-def concatenate_images(images, strategy, dist_images):
+def concatenate_images(images, strategy, dist_images, grid_resolution):
     if strategy == 'vertical':
         return concatenate_images_vertical(images, dist_images)
     elif strategy == 'horizontal':
         return concatenate_images_horizontal(images, dist_images)
     elif strategy == 'grid':
-        return concatenate_images_grid(images, dist_images)
+        return concatenate_images_grid(images, dist_images, grid_resolution)
     else:
         raise ValueError("Invalid concatenation strategy specified")
+
 
 def main(args):
     disable_torch_init()
@@ -131,8 +170,9 @@ def main(args):
     else:
         roles = conv.roles
 
+    args.images = expand_image_range_paths(args.images)
     images = [load_image(img_file) for img_file in args.images]
-    image = concatenate_images(images, args.concat_strategy, args.dist_images) if len(images) > 1 else images[0]
+    image = concatenate_images(images, args.concat_strategy, args.dist_images, args.grid_resolution) if len(images) > 1 else images[0]
     image_size = image.size
 
     if args.save_image:
@@ -205,7 +245,7 @@ if __name__ == "__main__":
     parser.add_argument("--debug", action="store_true")
 
     parser.add_argument("--images", type=str, nargs='+', required=True,
-                    help="Specify the paths for images to be concatenated. Accepts multiple paths.")
+                    help="Specify the paths for images to be concatenated. Accepts multiple paths, or range of images in the same location, e.g. img{1-4}.jpg.")
 
     parser.add_argument("--save-image", action="store_true",
                     help="If used, stores the resulting concatenated image in the LLaVA directory as 'concat-image.jpg'.")
@@ -215,6 +255,9 @@ if __name__ == "__main__":
     
     parser.add_argument("--dist-images", type=int, default=20,
                     help="Sets the spacing (in pixels) between concatenated images.")
+    
+    parser.add_argument("--grid-resolution", type=parse_resolution, default='2560,1440', 
+                        help="Fixed resolution of the resulting grid image. Specify as width, height. Default is 2560,1440.")
 
     args = parser.parse_args()
     main(args)
